b14f84e4168a6869045aa87289334f73837f2b7a
pavankumar pammi <pammimanikantapavankumar@gmail.com>
2023-07-04 21:02:45 +0530

    main

---
+++ Diff_var.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Diff_var {
+   public static void main(String[] args) {
+      Scanner ob =new Scanner(System.in);
+      int a;
+      double b;
+      
+      System.out.println("Enter a integer value:");
+      a = ob.nextInt();
+      System.out.println("Enter a Double value:"); 
+      b = ob.nextDouble();
+      System.out.println("Enter a string value:");
+      String c = "java";
+      System.out.println(a+" "+b+" "+c);
+
+   }
+}
